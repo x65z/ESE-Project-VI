e6d0d12517960d9bbd0bf36cf55519d9a66a6459
Hriaz5962 <39711553+Hriaz5962@users.noreply.github.com>
2018-07-23 16:52:00 -0400

    members update

--- Website/php/members.php
+++ Website/php/members.php
@@ -2,12 +2,18 @@
   //member.php
 	session_start(); //starts a session and creates a session variable
 
-if (isset($_SESSION['username'])){
-  echo "Welcome, " . $_SESSION['username'] . "!</br>";
+if (isset($_SESSION['logged_in'])){
+  echo "Welcome, " . $_SESSION['sUser'] . "!</br>";
   //Members only content
-  echo "<p>members only content - for your eyes only.</p>";
-
+  echo "<p>Wrapper for Elevator status wip</p>";
   echo "Click to <a href='logout.php'>Logout</a>";
+  echo "Click here to look at the debugg/log page <a href='debugger.php'>Debuger</a>";
+  
+  
+	//require '../html/Elevator.html';
+	require 'ElevatorContainer.php';
+  
+  
 }
 
 else{
